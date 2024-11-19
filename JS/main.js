6c41142233d9959425ee7b91e970bb228b8fdfec
PC13 <marceloviedma2008@cesanrafael.org>
2024-11-19 19:25:02 +0100

    faby

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+function suma(){
+    let numerouno=2;
+    let numerodos=10
+    let numerotres=4;
+    let resultado=numerouno+numerodos+numerotres;
+    alert("la suma es "+ resultado);
+}
+function resta(){
+    let numerouno=2;
+    let numerodos=10
+    let numerotres=4;
+    let resultado=numerouno-numerodos-numerotres;
+    alert("la resta es "+ resultado);
+}
+function multiplicacion(){
+    let numerouno=2;
+    let numerodos=10
+    let numerotres=4;
+    let resultado=numerouno*numerodos*numerotres;
+    alert("la multiplicacion es"+ resultado);
+}
+function division(){
+    let numerouno=2;
+    let numerodos=10
+    let numerotres=4;
+    let resultado=numerouno/numerodos/numerotres;
+    alert("la division es"+ resultado);
+}
